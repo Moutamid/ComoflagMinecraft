89984d533cb3eac2be6a7f2e806849e1061b7a43
Moutamid <lumianokia188@gmail.com>
2023-09-04 17:16:56 +0500

    chnage

--- app/src/main/java/com/moliyacentre/topcamouflageskinsmcpe/utils/ConstantFunctions.java
+++ app/src/main/java/com/moliyacentre/topcamouflageskinsmcpe/utils/ConstantFunctions.java
@@ -10,6 +10,10 @@ import com.moliyacentre.topcamouflageskinsmcpe.myapplication.Myapplication;
 
 
 public class ConstantFunctions {
+
+
+    public static final String LICENSE_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArBueEiTWfZk7O+BE104DZiZh6Dj8tIMJdZter0tO2KH0MUzn1UB14nrg6bb5IGup29NEvG6UI0ZelHgxClkYvLxzgXjagFgFKN46U6kijHxsccY9evn06NWNyJbPFoAkTQyVIPk44SBufx7g4H5f0azOtY28DL3fg5nvLDJ7yPAejGSvdZXuiVGopwS1A05QrjrgA6ol1YOUzxu22Vanb4ncIDTdF35MA2arbVf74fYFcqkJgWcfWkENRDxoj8IPo1tzWH2rO1/vaILmosJKd1SOrMhrmmyLita0AzJXH/d59Gwu3ed53Ct/Qcq9bDIX3TOALPdQ+NaRkapT2w0twQIDAQAB";
+    public static final  String VIP_MONTH = "vip.month.com.moliyacentre.topcamouflageskinsmcpe";
     public static RequestOptions requestOptions = new RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL);
 
     public static void loadImage(String str, ImageView imageView) {
